eb90c58b62f598061e3849d1ae032f0f6a55bc31
parkrocket <shinbyro@kakao.kr>
2023-04-07 11:00:38 +0900

    배열추가

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const school = (name) => {
 
-    const class_name = ["햇님반","달님반","별님반","지구반","바다반"];
+    const class_name = ["햇님반","달님반","별님반","지구반","바다반","지구방위대","나무반","코드반"];
 
     const randomValue = class_name[Math.floor(Math.random() * class_name.length)];
 
